58f699616138fd567eb88f00e0750294cfa2379c
Dario <67426199+dariosella@users.noreply.github.com>
2025-09-26 13:05:25 +0200

    Update header.h
    aggiunta funzione handle per recv che controlla se la connessione è stata chiusa

--- header.h
+++ header.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
 #define LISTENQ (8)
 #define BUF_SIZE (256)
 #define NAME_SIZE (64)
@@ -5,4 +9,29 @@
 #define USR_SIZE (64)
 #define PWD_SIZE (64)
 
+#define SERVER 0
+#define CLIENT 1
 
+void handle(int res, int sock, int who){
+	if (res == 0){
+		// la connessione è stata chiusa
+		close(sock);
+		if (who == SERVER){
+			puts("Connessione chiusa");
+			pthread_exit(NULL);
+		} else if (who == CLIENT){
+			puts("Connessione chiusa");
+			exit(EXIT_SUCCESS);
+		}
+	} else if (res == -1){
+		// errore
+		perror("recv");
+		if (who == SERVER){
+			puts("Errore");
+			pthread_exit(NULL);
+		} else if (who == CLIENT){
+			puts("Errore");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
