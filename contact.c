a22d54ed4a515bc4da0127b2d69b7903b2495487
Dario <67426199+dariosella@users.noreply.github.com>
2025-10-05 15:24:44 +0200

    Update contact.c

--- contact.c
+++ contact.c
@@ -97,7 +97,7 @@ void searchContact(Contact *contatto, char *answer){
 			if (temp == NULL){
 				perror("malloc");
 				strcpy(answer, "Errore di memoria insufficente\n");
-    close(fd);
+                close(fd);
 				return; // errore
 			}
 			
